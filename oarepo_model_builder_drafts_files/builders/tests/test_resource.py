3f34d31882a778dcac836cf0c85e67e451b5d613
Ronald Krist <ronald.krist@cesnet.cz>
2023-09-19 08:40:30 +0200

    format

--- oarepo_model_builder_drafts_files/builders/tests/test_resource.py
+++ oarepo_model_builder_drafts_files/builders/tests/test_resource.py
@@ -8,7 +8,9 @@ class TestResourcesBuilder(InvenioFilesParentBuilder):
     def finish(self, **extra_kwargs):
         tests = getattr(self.current_model, "section_tests")
         super().finish(
-            file_record=self.current_model.file_record.definition, test_constants=tests.constants, **extra_kwargs
+            file_record=self.current_model.file_record.definition,
+            test_constants=tests.constants,
+            **extra_kwargs,
         )
 
     def _get_output_module(self):
